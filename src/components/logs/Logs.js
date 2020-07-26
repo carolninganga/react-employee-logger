8aded22b75db3c4f52852a6f9188de7fdf6f2efa
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-26 11:59:35 -0400

    added logitem to logs file

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,4 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import LogItem from './LogItem';
+
+
 const Logs = () => {
     const [logs, setLogs] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -17,12 +20,12 @@ const Logs = () => {
         return <h4>Loading</h4>
     }
     return (
-        <ul className="collection-with-header">
+        <ul className="collection with-header">
         <li className="collection-header">
         <h4 className="center">System Logs</h4>
         </li>
         {!loading && logs.length === 0? (<p className="center">No logs to show...</p>):(
-            logs.map(log => <li>{log.message}</li>) 
+            logs.map(log => <LogItem log={log} key={log.id}/>) 
         )}
         </ul>
     )
