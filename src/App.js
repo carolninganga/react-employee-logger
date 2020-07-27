6215b2c112076d7e4428db54e183f5cd51569d9b
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-27 15:18:02 -0400

    added Searchbar fiile

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import React, { useEffect, Fragment } from 'react';
-import 'materialize-css/dist/css/materialize.min.css';
+import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
-import AddBtn from './components/layout/AddBtn';
-import AddLogModel from './components/logs/AddLogModal';
-import EditLogModal from './components/logs/EditlogModal';
-import AddTechModel from './components/techs/AddTechModal';
-
 import Logs from './components/logs/Logs';
-import M from 'materialize-css/dist/js/materialize';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
 const App = () => {
   useEffect(() => {
-    //Init Materialize JS
+    // Init Materialize JS
     M.AutoInit();
-  })
+  });
   return (
-    <Fragment>
-      <SearchBar />
-      <div className='container'>
-        <AddBtn />
-        <AddLogModel />
-        <EditLogModal />
-        <AddTechModel />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
-}
+};
 
 export default App;
